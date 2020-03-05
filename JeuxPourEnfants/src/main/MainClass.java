d23566860c8b72868f4d5cb1376a626c34cd665c
Khalid <khalid.abdelk@gmail.com>
2020-03-05 09:11:57 +0100

    Ajout fonctionnalite

--- JeuxPourEnfants/src/main/MainClass.java
+++ JeuxPourEnfants/src/main/MainClass.java
@@ -15,7 +15,7 @@ public class MainClass {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Ca foncitonne ");
     }
 
 }
