6ac7e6b0b8c9b6063e592681545114be0ef7e6a6
Khalid <khalid.abdelk@gmail.com>
2020-03-06 08:46:56 +0100

    Class Calul lvl 1 ok !

--- JeuxPourEnfants/src/main/Calcul.java
+++ JeuxPourEnfants/src/main/Calcul.java
@@ -5,6 +5,8 @@
  */
 package main;
 
+import java.util.Random;
+
 /**
  *
  * @author Khalid <Khalid.Khalid at ldnr.fr>
@@ -14,8 +16,22 @@ public class Calcul {
     private int x;
     private int y;
     private int resultat;
-    private final int MIN = 1;
-    private final int MAX = 10;
+    private final String operande;
+
+    public Calcul() {
+
+        this.x = aleaChiffre();
+        this.operande = aleaOperation();
+        this.y = aleaY();
+
+        this.resultat = resultat();
+
+    }
+
+    @Override
+    public String toString() {
+        return "Calcul{ " + x + operande + y + " = " + resultat + '}';
+    }
 
     public int getX() {
         return x;
@@ -41,39 +57,40 @@ public class Calcul {
         this.resultat = resultat;
     }
 
-    public void aleaChiffre() {
-
-        y = MIN + (int) (Math.random() * ((MAX - MIN) + 1));
-
-        x = MIN + (int) (Math.random() * ((MAX - MIN) + 1));
-
+    private int aleaChiffre() {
+        Random r = new Random();
+        int chiffre = (r.nextInt(10));
+        return chiffre;
     }
 
-    @Override
-    public String toString() {
-        return "Calcul{" + "x=" + x + ", y=" + y + ", resultat=" + resultat + ", MIN=" + MIN + ", MAX=" + MAX + '}';
-    }
+    private int aleaY() {
 
-    public void aleaCalcul() {
-        Calcul c = new Calcul();
-        c.aleaChiffre();
-
-        int alea = 1 + (int) (Math.random() * ((2 - 1) + 1));;
-
-        if (alea == 1) {
-            setResultat(c.x + c.y);
-            System.out.println(c.x + " + " + c.y + " = ");
-        } else {
-
-            while (c.x < c.y) {
-                c.aleaChiffre();
-
+       y=aleaChiffre();
+        if ("-".equals(this.operande)) {
+            while (y > x) {
+                this.y = aleaChiffre();
             }
-            setResultat(c.x - c.y);
-            System.out.println(c.x + " - " + c.y + " = ");
+        }
+        return y;
+    }
 
+    private static String aleaOperation() {
+        Random r = new Random();
+        int chiffre = (r.nextInt(2));
+        if (chiffre == 1) {
+            return "+";
+        } else {
+            return "-";
         }
 
+    }
+
+    private int resultat() {
+        if (operande.equals("+")) {
+            return x + y;
+        } else {
+            return x - y;
+        }
     }
 
 }
