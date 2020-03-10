47dd49fc92b4ff66c5307c57dec66d38ed3d7e3d
mehdi <mehdittal@protonmail.com>
2020-03-10 17:52:35 +0100

    ecouteur pop-up et id dans fichier mdp.txt

--- JeuxPourEnfants/src/containrs/ConnexionAdmin.java
+++ JeuxPourEnfants/src/containrs/ConnexionAdmin.java
@@ -1,5 +1,16 @@
 package containrs;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -25,30 +36,66 @@ public class ConnexionAdmin extends JFrame {
 
         panel.setLayout(null);
 
+        //label message a l'utilisateur
+        JLabel error = new JLabel();
+        error.setFont(new Font("Courier New", Font.BOLD, 13));
+        panel.add(error);
+        error.setForeground(Color.BLUE);
+        error.setText("Entrez vos informations");
+        error.setBounds(100, 10, 250, 25);
+
         //label utilisateur
         JLabel userLabel = new JLabel("Utilisateur");
-        userLabel.setBounds(10, 20, 150, 25);
+        userLabel.setBounds(10, 40, 150, 25);
         panel.add(userLabel);
 
         // champs pour entrer l'utilisateur
         JTextField userText = new JTextField(20);
-        userText.setBounds(150, 20, 165, 25);
+        userText.setBounds(150, 40, 165, 25);
         panel.add(userText);
 
         //label mot de passe
         JLabel passwordLabel = new JLabel("Mot de passe");
-        passwordLabel.setBounds(10, 50, 150, 25);
+        passwordLabel.setBounds(10, 70, 150, 25);
         panel.add(passwordLabel);
 
         // champs pour entrer le mot de passe
         JPasswordField passwordText = new JPasswordField(20);
-        passwordText.setBounds(150, 50, 165, 25);
+        passwordText.setBounds(150, 70, 165, 25);
         panel.add(passwordText);
 
         // bouton se connecter
         JButton loginButton = new JButton("Se connecter");
-        loginButton.setBounds(10, 80, 130, 25);
+        loginButton.setBounds(10, 100, 130, 25);
         panel.add(loginButton);
-    }
 
+        try (FileReader fileRead = new FileReader("mdp.txt")) {
+            // Crée un objet properties
+            Properties adminProps = new Properties();
+            // Charge les properties depuis un fichier
+            adminProps.load(fileRead);
+
+            loginButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (userText.getText().equals(adminProps.getProperty("user"))
+                            && passwordText.getText().equals(adminProps.getProperty("password"))) {
+
+                        OngletAdmin admin = new OngletAdmin();
+                        frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+
+                    } else {
+                        error.setFont(new Font("Courier New", Font.ITALIC, 13));
+                        error.setForeground(Color.RED);
+                        error.setText("Identifiant ou mot de passe incorrect");
+                    }
+                }
+            });
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ConnexionAdmin.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ConnexionAdmin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
